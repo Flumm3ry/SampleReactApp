e138028183f129eb02a27ec6df983315cf37c42c
jmaxwell_acceleon <jmaxwell@acceleon.com.au>
2020-02-04 12:06:23 +1100

    eslint errors fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { data } from './data/sampleData';
 import CarTable from './components/CarTable';
 
-const App = () => {
-  return (
-    <>
-      <h1>Hello, World!</h1>
-      <CarTable data={data} />
-    </>
-  );
-}
+const App = () => (
+  <>
+    <h1>Hello, World!</h1>
+    <CarTable data={data} />
+  </>
+);
 
 export default App;
