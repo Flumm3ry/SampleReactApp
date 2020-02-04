e97ae1e863a592b5ed18815cbb2c61a974c50dba
jmaxwell_acceleon <jmaxwell@acceleon.com.au>
2020-02-04 13:45:54 +1100

    Add test showing table rendering right num of rows

---
+++ src/components/CarTable.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CarTable from './CarTable';
+import { data } from './../data/sampleData';
+
+describe('Car table', () => {
+    it('Contains the right number of rows', () => {
+        const testTable = shallow(<CarTable data={data} />);
+        const rows = testTable.find('CarTableRow');
+        expect(rows.length).toEqual(data.length);
+    })
+});
